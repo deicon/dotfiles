0ed4e33b5d62e287944f14099fa95ede7af64b41
Dieter Home <eickstaedt@deicon.de>
2019-03-10 17:43:33 +0100

    Fixing setup. Make sure to kill all emacs daemons

--- .emacs.d.fromscratch/dotemacs.el
+++ .emacs.d.fromscratch/dotemacs.el
@@ -1,4 +1,17 @@
 
+(menu-bar-mode -1)
+(toggle-scroll-bar -1)
+(tool-bar-mode -1)
+
+(use-package zenburn-theme
+  :ensure t)
+
+(use-package anti-zenburn-theme
+  :ensure t)
+
+(use-package solarized-theme
+  :ensure t)
+
 (use-package flycheck
   :ensure t
   :config
@@ -8,21 +21,53 @@
   )
 
 (use-package projectile
-    :ensure t
-    :config
-    (progn
-       (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
-       (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
-       (define-key projectile-mode-map (kbd "M-n") 'projectile-find-file)
-      )
-    )
-(projectile-mode +1)
+        :ensure t
+        :config
+        (progn
+           (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
+           (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+           (define-key projectile-mode-map (kbd "M-n") 'projectile-find-file)
+           (recentf-mode)
+          )
+        )
+    (projectile-mode +1)
+
+(use-package neotree
+  :ensure t 
+  :bind (("<f8>" . neotree-toggle))
+  )
 
 (use-package org-bullets
     :ensure t
     :config
     (add-hook 'org-mode-hook 'org-bullets-mode)
 )
+
+(custom-set-variables
+      '(org-directory "~/Dropbox/deicon/20. Orgfiles")
+      '(org-default-notes-file (concat org-directory "/notes.org"))
+      '(org-export-html-postamble nil)
+      '(org-hide-leading-stars t)
+      '(org-startup-folded (quote overview))
+      '(org-startup-indented t)
+      )
+
+(global-set-key "\C-ca" 'org-agenda)
+
+(setq org-agenda-files (list "~/Dropbox/Deicon/20. Orgfiles/schedule.org"
+                             "~/Dropbox/Deicon/20. Orgfiles/ideas.org"
+                             "~/Dropbox/Deicon/20. Orgfiles/links.org"))
+
+(setq org-capture-templates
+      '(
+        ("t" "TODO" entry (file+headline "~/Dropbox/Deicon/20. Orgfiles/notes.org" "Neue Todos")
+         "* TODO %?\n %i\n %a")
+        ("l" "Link" entry (file+headline "~/Dropbox/Deicon/20. Orgfiles/links.org" "Neuer Link")
+         "* %? %^L %^g \n%T" :prepend t)
+        )
+     ) 
+
+(global-set-key "\C-cc" 'org-capture)
 
 (use-package avy
   :ensure t
@@ -67,3 +112,28 @@
 (use-package helm-projectile
   :ensure t
   )
+
+(global-set-key "\C-ce" 'recentf-open-files)
+
+(global-set-key "\C-xb" 'ibuffer)
+
+(show-paren-mode)
+
+(use-package magit
+    :ensure t
+    )
+(global-set-key (kbd "C-x g") 'magit-status)
+
+(use-package plantuml-mode
+  :ensure t
+      )
+
+;; active Org-babel languages
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '(;; other Babel languages
+   (plantuml . t)))
+
+(setq org-plantuml-jar-path
+      (expand-file-name "~/plantuml.jar")
+      )
--- .emacs.d.fromscratch/init.el
+++ .emacs.d.fromscratch/init.el
@@ -31,6 +31,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("73c69e346ec1cb3d1508c2447f6518a6e582851792a8c0e57a22d6b9948071b4" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "d057f0430ba54f813a5d60c1d18f28cf97d271fd35a36be478e20924ea9451bd" default)))
  '(org-default-notes-file (concat org-directory "/notes.org"))
  '(org-directory "~/Dropbox/deicon/20. Orgfiles")
  '(org-export-html-postamble nil)
@@ -39,7 +42,8 @@
  '(org-startup-indented t)
  '(package-selected-packages
    (quote
-    (magit web-mode undo-tree org-bullet which-key use-package try org-plus-contrib org-bullets flycheck evil delight counsel company auto-complete))))
+    (plantuml-mode anti-zenburn-theme solarized-theme zenburn-theme neotree treemacs treemacs-projectile magit web-mode undo-tree org-bullet which-key use-package try org-plus-contrib org-bullets flycheck evil delight counsel company auto-complete)))
+ '(show-paren-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
